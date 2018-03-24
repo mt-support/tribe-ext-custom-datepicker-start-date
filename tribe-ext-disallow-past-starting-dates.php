05578a837c185bea996056709ab1a5e6ea4f8335
Clifford Paulick <tko@tourkick.com>
2018-03-24 08:16:14 -0500

    Add github updater to header

--- tribe-ext-disallow-past-starting-dates.php
+++ tribe-ext-disallow-past-starting-dates.php
@@ -1,14 +1,15 @@
 <?php
 /**
- * Plugin Name:     The Events Calendar Extension: Disallow Past Starting Dates
- * Description:     Disallow selecting a past starting date for an event's start date datepicker in the wp-admin event edit screen and the Community Events form (if installed).
- * Version:         1.0.0
- * Extension Class: Tribe__Extension__Disallow_Past_Start_Dates
- * Author:          Modern Tribe, Inc.
- * Author URI:      http://m.tri.be/1971
- * License:         GPL version 3 or any later version
- * License URI:     https://www.gnu.org/licenses/gpl-3.0.html
- * Text Domain:     tribe-ext-disallow-past-starting-dates
+ * Plugin Name:       The Events Calendar Extension: Disallow Past Starting Dates
+ * Description:       Disallow selecting a past starting date for an event's start date datepicker in the wp-admin event edit screen and the Community Events form (if installed).
+ * Version:           1.0.0
+ * Extension Class:   Tribe__Extension__Disallow_Past_Start_Dates
+ * GitHub Plugin URI: https://github.com/mt-support/tribe-ext-disallow-past-starting-dates
+ * Author:            Modern Tribe, Inc.
+ * Author URI:        http://m.tri.be/1971
+ * License:           GPL version 3 or any later version
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       tribe-ext-disallow-past-starting-dates
  *
  *     This plugin is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
